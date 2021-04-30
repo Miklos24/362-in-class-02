0510e83e804dd1de52b894b37208826f223152f0
Miklos Bowling <miklos.bowling@gmail.com>
2021-04-29 17:03:30 -0700

    Extra tests and a README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 362-in-class-02
+
+Run `python3 testCalculator.py` to perform tests.
--- testCalculator.py
+++ testCalculator.py
@@ -5,16 +5,22 @@ class TestCase(unittest.TestCase):
     def test_add_basic(self):
         self.assertEqual(5, calculator.add(2, 3))
     
-    def test_add_error(self):
+    def test_add_val_error(self):
         self.assertRaises(ValueError, calculator.add("fish", 3))
     
-    def test_subtract(self):
+    def test_subtract_basic(self):
+        self.assertEqual(1, calculator.subtract(3, 2))
+
+    def test_subtract_neg(self):
         self.assertEqual(-1, calculator.subtract(2, 3))
     
-    def test_multiply(self):
+    def test_multiply_basic(self):
         self.assertEqual(6, calculator.multiply(2, 3))
+
+    def test_multiply_list(self):
+        self.assertEqual([4, 6], calculator.multiply(2, [2, 3]))
     
-    def test_divide_int(self):
+    def test_divide_basic(self):
         self.assertEqual(2, calculator.divide(8, 4))
     
     def test_divide_float(self):
